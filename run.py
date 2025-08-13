d605cf3512835832ad7be94e3f8881f140ca3644
omridinio <solomonom@mta.ac.il>
2025-08-13 17:41:22 +0300

    bug fix

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ app = create_app()
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8574))
     # Only use debug mode locally, not in production
-    debug_mode = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
+    #debug_mode = os.environ.get("FLASK_DEBUG", "false").lower() == "true"
     app.run(host="0.0.0.0", port=port, debug=False)
